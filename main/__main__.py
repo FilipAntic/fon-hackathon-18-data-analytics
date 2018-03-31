e30acd37632353f6007a74b3f40462ca7f9002fe
Filip <antic.filip996@gmail.com>
2018-03-31 11:42:10 +0200

    first commit

---
+++ main/__main__.py
@@ -0,0 +1,28 @@
+
+
+if __name__ == '__main__':
+
+    # --------matplotlib i numpy primeri ----------
+    # import matplotlib.pyplot as plt
+    # import numpy as np
+    #
+    # t = np.arange(0.0, 2.0, 0.01)
+    # s = 1 + np.sin(2 * np.pi * t)
+    # plt.plot(t, s)
+    #
+    # plt.xlabel('time (s)')
+    # plt.ylabel('voltage (mV)')
+    # plt.title('About as simple as it gets, folks')
+    # plt.grid(True)
+    # plt.savefig("test.png")
+    # plt.show()
+
+    #---------------- pandas primer---------
+    # import pandas as pd
+    # data = pd.read_csv("TechCrunchcontinentalUSA.csv")
+    # print(data.head()) #ispisuje prvih 5 iz csv fajla
+
+
+
+
+    pass
